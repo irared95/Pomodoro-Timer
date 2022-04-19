27cf8c84d8003a04dc395d1c8d67ac19f1c4db23
Ira Redchets <irared95@gmail.com>
2022-04-19 23:26:28 +0300

    markup slider

---
+++ src/script/slider.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    $('.slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        speed: 300,
+        arrows: false,
+        dots: false,
+        centerMode: true,
+
+    });
+
+
+
+})
