33dd4abad27a562774848e70dc38facbb175849e
Ira Redchets <irared95@gmail.com>
2022-04-21 22:56:11 +0300

    timer

--- src/script/timer.js
+++ src/script/timer.js
@@ -1,36 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
-    const play = document.querySelector('.play--js');
-    const stop = document.querySelector('.stop--js');
-
-    play.addEventListener('click', function () {
-        this.classList.toggle('active');
-        stop.classList.toggle('active')
-    })
-
-    stop.addEventListener('click', function (){
-        this.classList.toggle('active');
-        play.classList.toggle('active')
-
-    })
-
-})
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    $('.slider').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: false,
-        speed: 300,
-        arrows: false,
-        dots: false,
-        centerMode: true,
-    });
-
-
-})
-document.addEventListener('DOMContentLoaded', function () {
     let minute = 0;
     let second = 0;
     let cron;
